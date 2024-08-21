8402ffb58c52f31172c126f197e519f32c722cc6
Chi Ho Ho <harushochiho@gmail.com>
2024-08-21 13:25:44 -0400

    Fixing the bug: JavaSbProduct shut down immediately

--- JavaSB/src/main/java/Configurations/DataSource.java
+++ JavaSB/src/main/java/Configurations/DataSource.java
@@ -5,21 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
-public class DataSource {
-
-    @Bean
-    public javax.sql.DataSource dataSource() {
-        DriverManagerDataSource source = new DriverManagerDataSource();
-        source.setDriverClassName("org.postgresql.Driver");
-        source.setUrl("jdbc:postgresql://host.docker.internal:5432/testing");
-        source.setUsername("user");
-        source.setPassword("password");
-        return source;
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
-        return new NamedParameterJdbcTemplate(this.dataSource());
-    }
-}
+//@Configuration
+//public class DataSource {
+//
+//    @Bean
+//    public javax.sql.DataSource dataSource() {
+//        DriverManagerDataSource source = new DriverManagerDataSource();
+//        source.setDriverClassName("org.postgresql.Driver");
+//        source.setUrl("jdbc:postgresql://host.docker.internal:5432/testing");
+//        source.setUsername("user");
+//        source.setPassword("password");
+//        return source;
+//    }
+//
+//    @Bean
+//    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
+//        return new NamedParameterJdbcTemplate(this.dataSource());
+//    }
+//}
--- JavaSbProduct/src/main/java/com/example/JavaSbProduct/JavaSbProductApplication.java
+++ JavaSbProduct/src/main/java/com/example/JavaSbProduct/JavaSbProductApplication.java
@@ -2,8 +2,10 @@ package com.example.JavaSbProduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JavaSbProductApplication {
 
 	public static void main(String[] args) {
