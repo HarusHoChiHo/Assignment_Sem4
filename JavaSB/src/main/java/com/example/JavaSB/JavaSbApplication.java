b5ec4df9bf58f05eaf027f856794a4d05d5b5a3f
Chi Ho Ho <cho127@my.centennialcollege.ca>
2024-08-21 16:47:48 -0400

    Fixed bug of JavaSbProduct

--- JavaSB/src/main/java/com/example/JavaSB/JavaSbApplication.java
+++ JavaSB/src/main/java/com/example/JavaSB/JavaSbApplication.java
@@ -8,7 +8,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class JavaSbApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(JavaSbApplication.class, args);
+
 	}
+
 
 }
